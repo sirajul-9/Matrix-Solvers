d9b674517ce3439c42ccde9d071f93036b98834a
Md Sirajul Basir <172808397+sirajul-9@users.noreply.github.com>
2025-11-03 20:03:41 +0530

    Added header file for the helper functions

---
+++ OpenMP_codes/funcs.h
@@ -0,0 +1,5 @@
+void mul_mat_vec(double*,double* , double*,int);
+double mul_vec_vec(double *, double *,int);
+void read_2d(char *, double *, int);
+void read_1d(char *, double *,int);
+void transpose(double *,double *,int);
